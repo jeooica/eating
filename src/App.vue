7d64653c23e6e46deb55f0c08101c76fd94c9530
daichanghong <daichanghong@jiuqi.com.cn>
2021-06-25 17:25:28 +0800

    今天中午吃什么？

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <n-button>Test</n-button>
+  <home-page></home-page>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePage from './views/HomePage.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {HomePage}
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    今天中午吃什么?
+  </div>
+</template>
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
