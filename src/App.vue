c777be3d1d806456d4ab896934207067f824c9fa
guoxun <guoxun@jiuqi.com.cn>
2021-06-25 14:32:36 +0800

    importing vite & naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <n-button>Test</n-button>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import naive from 'naive-ui'
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(naive).mount('#app')
